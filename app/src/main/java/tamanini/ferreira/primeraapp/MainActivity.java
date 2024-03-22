e9a7c73d1959f84da9bc250e6f572aa3afe50048
Anna Júlia <annajulia.tamanini@gmail.com>
2024-03-22 09:10:06 -0300

    Comentando o código

--- app/src/main/java/tamanini/ferreira/primeraapp/MainActivity.java
+++ app/src/main/java/tamanini/ferreira/primeraapp/MainActivity.java
@@ -13,16 +13,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnEnviar = findViewById(R.id.btnEnviar);
+        setContentView(R.layout.activity_main); //Ler o arquivo de layot e constroi a interface
+        Button btnEnviar = findViewById(R.id.btnEnviar); //Obtem o botao de enviar
+        //defini a açao do click no botao
         btnEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
+            @Override //o onclick e chamado quando da o click do botao
             public void onClick(View v) {
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-                String textoDigitado = etDigiteAqui.getText().toString();
-                Intent i = new Intent(MainActivity.this,NextActivity.class);
-                i.putExtra("texto", textoDigitado);
-                startActivity(i);
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //para obter o campo de texto
+                String textoDigitado = etDigiteAqui.getText().toString(); //obtem o texto da caixa de texto
+                Intent i = new Intent(MainActivity.this,NextActivity.class); //cria uma intençao para navegar a proxima tela
+                i.putExtra("texto", textoDigitado); //coloca o texto dentro da intençao
+                startActivity(i); //executa a intençao e navega para a proxima tela
             }
         });
     }
--- app/src/main/java/tamanini/ferreira/primeraapp/NextActivity.java
+++ app/src/main/java/tamanini/ferreira/primeraapp/NextActivity.java
@@ -13,12 +13,12 @@ public class NextActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_next);
+        setContentView(R.layout.activity_next); //Ler o arquivo de layot e constroi a interface
 
 
-        Intent i = getIntent();
-        String texto = i.getStringExtra("texto");
-        TextView tvTexto = findViewById(R.id.tvTexto);
-        tvTexto.setText(texto);
+        Intent i = getIntent(); //obtem o intent que criou essa tela
+        String texto = i.getStringExtra("texto"); //obtem o texto que veio junto com o intent
+        TextView tvTexto = findViewById(R.id.tvTexto); //ele obtem o textview
+        tvTexto.setText(texto); //seta o texto dentro do textview
     }
 }
